b1a15020444390c97db9a10e6d504f6570f612aa
Wiebke Freitag <wiebke.freitag@satellytes.com>
2022-11-29 20:26:00 +0100

    feat: shell scripts

--- api/run-process.sh
+++ api/run-process.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
 
-echo "Start"
-curl -X POST https://cpee.org/flow/start/xml/ -F behavior=fork freitag -F xml=@test-process.xml
-echo "Finish"
+TRACES=${1:-5}
+BREAK="\n- - - - - - - - - - - - - - - -"
 
+for (( i=1; i<=$TRACES; i++ ))
+do
+    echo -e "Start of Trace $i"
+    curl -X POST https://cpee.org/flow/start/xml/ -F behavior=fork_running -F xml=@test-process.xml
+    if test $i != $TRACES
+    then
+        echo -e $BREAK
+    fi
+done
