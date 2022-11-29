c19fa1e275f583fb6060c6b7813242c3d3231eb0
Wiebke Freitag <wiebke.freitag@satellytes.com>
2022-11-29 22:18:02 +0100

    feat: save data to csv

--- api/main.py
+++ api/main.py
@@ -1,6 +1,9 @@
 from typing import Union
 from fastapi import Request, FastAPI
+import pandas as pd
 import json
+import csv
+import os.path
 
 app = FastAPI()
 
@@ -14,5 +17,18 @@ async def log(request: Request):
     all_infos = body_as_byte.decode('UTF-8').split("\r\n")
     body_as_string = [param for param in all_infos if param.startswith("{") and param.endswith("}")][0]
     body = json.loads(body_as_string)
-    print(body)
-    return {"item_id": "success"}
+    file_name = body['instance-name']
+    file_exists = os.path.exists('{0}.csv'.format(file_name))
+    if file_exists:
+        with open('{0}.csv'.format(file_name), 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(list(body.values()))      
+    else:
+        with open('{0}.csv'.format(file_name), 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(list(body.keys()))
+            writer.writerow(list(body.values())) 
+
+    df = pd.read_csv("data.csv")
+    print(df)
+    return {"trace": "success"}
