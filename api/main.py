010fac44832e2d77d470b2c1abca949a3ccc510e
WiebkeFr <ge38hiw@i17vm2.in.tum.de>
2022-11-29 20:21:41 +0100

    feat: process request body

--- api/main.py
+++ api/main.py
@@ -1,19 +1,18 @@
 from typing import Union
 from fastapi import Request, FastAPI
+import json
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"ppm-api": "is running..."}
 
 @app.post("/")
 async def log(request: Request):
-    param = await request.json()
-    print(param)
+    body_as_byte = await request.body()
+    all_infos = body_as_byte.decode('UTF-8').split("\r\n")
+    body_as_string = [param for param in all_infos if param.startswith("{") and param.endswith("}")][0]
+    body = json.loads(body_as_string)
+    print(body)
     return {"item_id": "success"}
