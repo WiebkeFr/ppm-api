4b232f7e52d00cf35fe705f931515607bbcd738c
WiebkeFr <ge38hiw@i17vm2.in.tum.de>
2022-11-29 22:29:12 +0100

    fix: replace file-name

--- api/main.py
+++ api/main.py
@@ -18,17 +18,18 @@ async def log(request: Request):
     body_as_string = [param for param in all_infos if param.startswith("{") and param.endswith("}")][0]
     body = json.loads(body_as_string)
     file_name = body['instance-name']
+    print(file_name)
     file_exists = os.path.exists('{0}.csv'.format(file_name))
     if file_exists:
         with open('{0}.csv'.format(file_name), 'a', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(list(body.values()))      
+            writer.writerow(list(body.values()))
     else:
         with open('{0}.csv'.format(file_name), 'a', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(list(body.keys()))
             writer.writerow(list(body.values())) 
 
-    df = pd.read_csv("data.csv")
+    df = pd.read_csv('{0}.csv'.format(file_name))
     print(df)
     return {"trace": "success"}
